f52884e9b2e378e1f2b64bf45a377f6585cba6f8
Jason Linden <jason.linden@lexisnexis.co.za>
2021-01-31 19:53:01 +0200

    added web api project

---
+++ TRMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            string username = RequestContext.Principal.Identity.GetUserName();
+
+            return new string[] { "value1", "value2", userId, username };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
