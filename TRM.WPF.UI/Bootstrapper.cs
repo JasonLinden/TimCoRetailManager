dafa2004435c437d6383d092875c08244a468bce
Jason Linden <jason.linden@lexisnexis.co.za>
2021-02-01 21:26:15 +0200

    added wpf project and Caliburn.Micro package

---
+++ TRM.WPF.UI/Bootstrapper.cs
@@ -0,0 +1,66 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRM.WPF.UI.ViewModels;
+
+namespace TRM.WPF.UI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(vmType => _container.RegisterPerRequest(vmType, vmType.ToString(), vmType));
+
+            //base.Configure();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+
+            // base.OnStartup(sender, e);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+
+            // return base.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+
+            // return base.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+
+            // base.BuildUp(instance);
+        }
+    }
+}
