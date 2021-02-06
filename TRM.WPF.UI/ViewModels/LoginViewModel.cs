df933e8bfcd50b48d84e6fccd6671a6f0e27f189
Jason Linden <jason.linden@lexisnexis.co.za>
2021-02-06 20:40:23 +0200

    login page

---
+++ TRM.WPF.UI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRM.WPF.UI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+
+        public string Username
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => Username);
+                NotifyOfPropertyChange(() => CanLogin);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogin);
+            }
+        }
+
+        public bool CanLogin
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(_userName) && !string.IsNullOrEmpty(_password);
+            }
+        }
+
+        public void Login()
+        {
+            Console.WriteLine();
+        }
+    }
+}
--- TRM.WPF.UI/ViewModels/ShellViewModel.cs
+++ TRM.WPF.UI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace TRM.WPF.UI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object> // object could be a interface or type which relates to a viewModel for a form
     {
+        private readonly LoginViewModel _loginViewModel;
+
+        public ShellViewModel(LoginViewModel loginViewModel)
+        {
+            _loginViewModel = loginViewModel;
+
+            ActivateItem(_loginViewModel);
+        }
     }
 }
